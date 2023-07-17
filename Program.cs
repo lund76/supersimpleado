c4dbf393cfec22c17a2cff6cf97be55ca0c8a8f7
Søren Lund <soren.lund@amesto.dk>
2023-07-17 14:16:40 +0200

    very simple ado.net example

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+
+namespace SuperSimpleADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Data Source=.\\sqlexpress;Initial Catalog=opgaveDB;Integrated Security=True";
+            var query = "SELECT Efternavn from P1 where Fornavn = @nameParameter";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@nameParameter", "Niels");
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader[0]); // replace 0 with the index of the column you want to read
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
