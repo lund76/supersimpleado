c8a9383160914028891f19d6e31e2185c3ab1c12
Søren Lund <soren.lund@amesto.dk>
2023-07-17 16:13:14 +0200

    using ef core

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.SqlClient;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlClient;
 
 namespace SuperSimpleADO
 {
@@ -6,8 +8,14 @@ namespace SuperSimpleADO
     {
         static void Main(string[] args)
         {
+
+
+            DoItWithEfCore();
+            Console.ReadKey();
+
+            return;
             var connectionString = "Data Source=.\\sqlexpress;Initial Catalog=opgaveDB;Integrated Security=True";
-            var query = "SELECT Efternavn from P1 where Fornavn = @nameParameter";
+            var query = "SELECT top 100 * from P1 where Fornavn = 'Niels' ";
 
             using (var connection = new SqlConnection(connectionString))
             {
@@ -15,13 +23,13 @@ namespace SuperSimpleADO
 
                 using (var command = new SqlCommand(query, connection))
                 {
-                    command.Parameters.AddWithValue("@nameParameter", "Niels");
+                    //command.Parameters.AddWithValue("@nameParameter", "Niels");
 
                     using (var reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            Console.WriteLine(reader[0]); // replace 0 with the index of the column you want to read
+                            Console.WriteLine(reader[1] + " " + reader[2]); // replace 0 with the index of the column you want to read
                         }
                     }
                 }
@@ -29,5 +37,44 @@ namespace SuperSimpleADO
 
             Console.ReadKey();
         }
+
+        public static void DoItWithEfCore()
+        {
+            using (var db = new PersonDatabaseContext())
+            {
+                var query =
+                    from row in db.Persons
+                    where row.Fornavn == "Niels"
+                    select row;
+
+                foreach (var row in query)
+                {
+                    Console.WriteLine(row.Efternavn + " " + row.PersonId.ToString());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public class PersonDatabaseContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.\\SqlExpress;Initial Catalog=opgaveDB;Integrated Security=True;Encrypt=false");
+        }
+    }
+
+    [Table("Person")]
+    public class Person
+    {
+        public int PersonId { get; set; } 
+        public string Fornavn { get; set; } 
+        public string Efternavn { get; set; }
+        public short Postnr { get; set; }
+        
     }
 }
